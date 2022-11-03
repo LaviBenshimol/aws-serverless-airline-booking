b473c7cc3a1eaab2695fd98c4e7f8c2aeefe0512
shachar58 <shaharfr58@gmail.com>
2022-11-03 16:18:27 +0200

    changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsserverlessairline44ea6421": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "api": {
+        "awsserverlessairline": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
